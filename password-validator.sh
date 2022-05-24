28afa4659f06519f949353d877ae1de8e93986c5
ItayDevops <itaygolan67@gmail.com>
2022-05-24 18:53:11 +0300

    final for main

--- password-validator.sh
+++ password-validator.sh
@@ -1,40 +1,38 @@
 #!/bin/bash
 ##input password for user check
-Red='\033[0;31m'      # Red
-Green='\033[0;32m'    # Green
-reset_color='\033[0m' # reset
-pass1=$1              # Takes Argument
-LEN=${#pass1}         #Password Len Check
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+reset_color='\033[0m'
+pass1=$1                  # Takes Argument
+LEN=${#pass1}             #Password Len Check
 
-function password_validetor() {
-
-    ###Check Password Length and its contains
-    if [[ $LEN -ge 10 ]]; then
-        if [[ $pass1 =~ [0-9] && $pass1 =~ [a-z] ]]; then
-            if [[ $pass1 =~ [A-Z] ]]; then
-                echo -e "Password Meet the Policy ${Green}$pass1 $reset_color" ##Highlight in Green When Ok
-                echo $?
-                exit 0
-            else
-                echo "you must enter Capital letters "
-                echo -e "Password Doesnt Meet the Policy ${Red}$pass1 $reset_color" ##Highlight in Red When Not Ok
-                echo $?
-                exit 1
-            fi
+function password_validetor () {
+###Check Password Length and its contains
+if [[ $LEN -ge 10 ]]
+then
+    if [[ $pass1 =~ [0-9] && $pass1 =~ [a-z] ]]
+    then
+        if [[ $pass1 =~ [A-Z] ]]
+        then
+            echo -e "Password Meet the Policy ${Green}$pass1 $reset_color" ##Highlight in Green When Ok
+            exit 0
+            
         else
-            echo "Password Contains special character"
+            echo " you must enter Capital letters "
             echo -e "Password Doesnt Meet the Policy ${Red}$pass1 $reset_color" ##Highlight in Red When Not Ok
-            echo $?
             exit 1
         fi
     else
-        echo "Password Doesnt Meet the Minimum Length"
+        echo "Password Contains special character"
         echo -e "Password Doesnt Meet the Policy ${Red}$pass1 $reset_color" ##Highlight in Red When Not Ok
-        echo $?
         exit 1
-
     fi
-
+else
+    echo "Password Doesnt Meet the Minimum Length"
+    echo -e "Password Doesnt Meet the Policy ${Red}$pass1 $reset_color" ##Highlight in Red When Not Ok
+    exit 1
+    
+fi
 }
 
 echo Hello User!
